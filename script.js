d205f5352b8ba693d204408ba32b703e80aec59b
lara <ajanakuomolara53@gmail.com>
2023-05-13 21:21:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function firstButton(){
+    window.open("https://github.com/Omolara-53")
+}
+
+function secondButton(){
+    window.open("https://twitter.com/omolara_ajanaku")
+}
+
+function thirdButton(){
+    window.open("https://www.linkedin.com/in/larajanaku/")
+}
+
+function forthButton(){
+    window.open("https://www.instagram.com/laraintech")
+}
+
+
